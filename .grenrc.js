7b9adfc2a8b3c5ac46dc9c9d5083b169d420090f
Lee Howett <lee@fingerfoodstudios.com>
2018-01-26 13:34:05 -0800

    added gren support

---
+++ .grenrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "dataSource": "prs",
+    "prefix": "v",
+    "ignoreLabels": ["duplicate", "good first issue", "help wanted", "invalid", "question", "ready for release", "wontfix"],
+    "onlyMilestones": false,
+    "groupBy": {
+        "Features": ["enhancement"],
+        "Bug Fixes": ["bug"]
+    },
+    template: {
+        label: function () { return "" },
+        release: function (placeholders) {
+          return `## ⛅[${placeholders.release.replace('cloud-', '')}](https://github.com/mayfieldrobotics/aws_cloud/releases/tag/${placeholders.release})\n${placeholders.body}`
+        }
+    },
+    "changelogFilename": "CHANGELOG.md"
+}
